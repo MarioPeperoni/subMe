f8943a7464e179adfa4c00a3dfb0fc51afafd27d
Mateusz Wiacek <mateuszwiacek2003@gmail.com>
2022-12-15 15:25:30 +0100

    Initial Commit

---
+++ sub/subApp.swift
@@ -0,0 +1,17 @@
+//
+//  subApp.swift
+//  sub
+//
+//  Created by Mateusz on 15/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct subApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
